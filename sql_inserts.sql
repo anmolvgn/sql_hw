89719f35af3c673beaf369a1b743c775819c92f6
Anmol Nuggehalli <anuggehalli93@gmail.com>
2017-10-16 20:30:53 -0600

    sql inserts

---
+++ sql_inserts.sql
@@ -0,0 +1,30 @@
+use b_amazon;
+
+SELECT * FROM b_amazon.products;
+
+insert into products (itemId, productName, deptName, priceUSD, stockQuant)
+values (12, "Samsung_Galaxy", "SAMSUNGProds", 199, 5000);
+
+insert into products (itemId, productName, deptName, priceUSD, stockQuant)
+values (13, "Nokia_Brick", "NokiaProds", 50, 500);
+
+insert into products (itemId, productName, deptName, priceUSD, stockQuant)
+values (14, "MacAir", "AppleProds", 1000, 50);
+
+insert into products (itemId, productName, deptName, priceUSD, stockQuant)
+values (15, "SurfacePro", "WindowsProds", 1500, 200);
+
+insert into products (itemId, productName, deptName, priceUSD, stockQuant)
+values (16, "Lenovo_IdeaPad", "IBMProds", 800, 1500);
+
+insert into products (itemId, productName, deptName, priceUSD, stockQuant)
+values (17, "Vizio_TV", "VizioProds", 600, 25);
+
+insert into products (itemId, productName, deptName, priceUSD, stockQuant)
+values (18, "DELL_INSPIRON", "DellProds", 350, 500);
+
+insert into products (itemId, productName, deptName, priceUSD, stockQuant)
+values (19, "iPhone_X", "AppleProds", 1000, 10);
+
+insert into products (itemId, productName, deptName, priceUSD, stockQuant)
+values (20, "LGPhone", "LGgProds", 250, 10000);
